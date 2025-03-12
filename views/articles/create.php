fa0e8f19247a805b147996821a0b425226b9c7c5
Martini <martin.jevsevar123@gmail.com>
2025-03-12 19:27:59 +0100

    Dopolnil create.php s kodo za objavo novice.

--- views/articles/create.php
+++ views/articles/create.php
@@ -1,3 +1,13 @@
 <div class="container">
-    <h3>Objavi novico</h3>
+    <h3 class="mb-3">Objavi novico</h3>
+    <form action="post.php" method="post">
+        <div class="mb-3">
+            <label for="novica" class="form-label">Napiši nekaj</label>
+            <input type="text" class="form-control" id="novica" name="novica" 
+                value="<?php echo isset($_POST["novica"]) ? htmlspecialchars($_POST["novica"], ENT_QUOTES, 'UTF-8') : ""; ?>">
+        </div>
+        <div class="mb-3">
+            <button type="submit" class="btn btn-primary" name="post">Objavi</button>
+        </div>
+    </form>
 </div>
